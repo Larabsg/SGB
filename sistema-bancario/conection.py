a52982bc5bd5294797696ad10f8e95193e1bc3f8
Larabsg <laragomes2009@hotmail.com>
2022-05-17 17:12:13 -0300

    conexão com banco de dados

---
+++ sistema-bancario/conection.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+con = mysql.connector.connect(host='localhost', database='sistema_bancario', user='admin', password='admin')
+
+if con.is_connected():
+    db_info = con.get_server_info()
+    print("Conectado ao servidor MySql versão ", db_info)
+    cursor = con.cursor()
+    cursor.execute('select database();')
+    linha = cursor.fetchone()
+    print("Conectado ao banco de dados ", linha)
+
+if con.is_connected():
+    cursor.close()
+    con.close()
+    print("Conexão ao MySQl foi encerrada")
