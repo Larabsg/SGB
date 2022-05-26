de6fa9a68c781499e832e42a3ded4955822b9faa
Larabsg <laragomes2009@hotmail.com>
2022-05-26 11:32:29 -0300

    estilizando tela de login

--- sistema-bancario/main.py
+++ sistema-bancario/main.py
@@ -11,13 +11,16 @@ from tkinter.ttk import *
 from tela_login import janelaLogin
 from tela_cadastro import janelaCadastrar
 
-laranja = "#ff4000"
-branco = "#feffff"
+c_pri = "#2d6375"
+branco = "#D7E0D7"
 
 preta = "#f0f3f5"
 verde = "#3fb5a3"
 letra = "#403d3d"
 valor = "#38576b"
+azul = "#00008e"
+c_sec = "#193842"
+
 
 def janelaPrincipal():
     janela = Tk()
@@ -25,23 +28,23 @@ def janelaPrincipal():
     janela.configure(background="#feffff")
     janela.resizable(width=FALSE, height=FALSE)
 
-    frame_cima = tkinter.Frame(janela, width=300, height=50, relief='flat', bg='#feffff')
-    frame_cima.grid(row=0, column=0, pady=1, padx=0, sticky=NSEW)
-    frame_baixo = tkinter.Frame(janela, width=300, height=190, relief='flat', bg='#feffff')
-    frame_baixo.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
+    frame_cima = tkinter.Frame(janela, width=300, height=50, relief='flat', bg=c_pri)
+    frame_cima.grid(row=0, column=0, pady=0, padx=0, sticky=NSEW)
+    frame_baixo = tkinter.Frame(janela, width=300, height=190, relief='flat', bg=c_pri)
+    frame_baixo.grid(row=1, column=0, pady=0, padx=0, sticky=NSEW)
 
     janela.title("Sistema Bancário")
 
-    texto = tkinter.Label(frame_cima, text="Bem Vindo!", anchor=NE, font=('Ivy', 20), bg=branco, fg=letra)
+    texto = tkinter.Label(frame_cima, text="Bem Vindo", anchor=NE, font=('Ivy', 18), bg=c_pri, fg=branco)
     texto.place(x=5, y =5)
 
-    linha = tkinter.Label(frame_cima, text="", anchor=NW, width=275, font=('Ivy 1'), bg=verde, fg=letra)
+    linha = tkinter.Label(frame_cima, text="", anchor=NW, width=275, font=('Ivy 1'), bg=branco, fg=letra)
     linha.place(x=10, y =45)
 
-    btn = tkinter.Button(frame_baixo, text="Fazer login", width=39, height=2, bg=verde, fg=branco, font=('Ivy 8 bold'), relief=RAISED, command=partial(janelaLogin, janela))
+    btn = tkinter.Button(frame_baixo, text="Fazer login", width=39, height=2, bg=c_sec, fg=branco, font=('Ivy 8 bold'), relief=FLAT, command= partial(janelaLogin, janela))
     btn.place(x=8, y =70)
 
-    btnCadastro = tkinter.Button(frame_baixo, text="Cadastrar-se", width=39, height=2, bg=verde, fg=branco, font=('Ivy 8 bold'), relief=RAISED, command=partial(janelaCadastrar, janela))
+    btnCadastro = tkinter.Button(frame_baixo, text="Cadastrar-se", width=39, height=2, bg=c_sec, fg=branco, font=('Ivy 8 bold'), relief=FLAT, command=partial(janelaCadastrar, janela))
     btnCadastro.place(x=8, y =120)
     
     # fechando a janela principal
--- sistema-bancario/tela_login.py
+++ sistema-bancario/tela_login.py
@@ -4,31 +4,54 @@ from tokenize import Double
 from connection_sqlite import *
 
 from tkinter import *
+import tkinter
 from tkinter.ttk import *
+from tkinter import messagebox
 import tela_incial
 
-def janelaLogin(janela, message=""):
+c_pri = "#2d6375"
+branco = "#D7E0D7"
+
+preta = "#f0f3f5"
+verde = "#3fb5a3"
+letra = "#403d3d"
+valor = "#38576b"
+azul = "#00008e"
+c_sec = "#193842"
+
+def janelaLogin(janela):
         janela10 = Toplevel(janela)
         janela10.title("Login")
-        janela10.geometry("300x300")
-        nconta = Label(janela10, text="Nº conta: ")
-        nconta.place(x=20, y =40)
+        janela10.geometry("310x300")
 
-        login = Entry(janela10, width=25)
-        login.place(x=90, y =40)
+        janela10.configure(background="#feffff")
+        janela.resizable(width=FALSE, height=FALSE)
 
-        senha = Label(janela10, text="Senha: ")
-        senha.place(x=20, y =70)
+        frame_cima = tkinter.Frame(janela10, width=300, height=50, relief='flat', bg='#feffff')
+        frame_cima.grid(row=0, column=0, pady=1, padx=0, sticky=NSEW)
+        frame_baixo = tkinter.Frame(janela10, width=300, height=250, relief='flat', bg='#feffff')
+        frame_baixo.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
 
-        passwd = Entry(janela10, width=25)
-        passwd.place(x=90, y =70)
+        texto = tkinter.Label(frame_cima, text="LOGIN", anchor=NE, font=('Ivy', 18), bg='#feffff', fg=c_pri)
+        texto.place(x=5, y =5)
 
-        btn = Button(janela10, text="Entrar", command=lambda: verifica_login(janela, login, passwd))
-        btn.place(x=40, y =150)
+        linha = tkinter.Label(frame_cima, text="", anchor=NW, width=275, font=('Ivy 1'), bg=c_sec, fg=letra)
+        linha.place(x=10, y =45)
 
-        if message != "":
-            messagem = Label(janela10, text=message)
-            messagem.place(x=20, y=190)
+        nconta = tkinter.Label(frame_baixo, text="Nº conta *", anchor=NW, font=('Ivy', 10), bg='#feffff', fg=c_pri)
+        nconta.place(x=10, y =20)
+
+        login = tkinter.Entry(frame_baixo, width=25, justify='left', font=("", 15), highlightthickness=1, relief='solid')
+        login.place(x=14, y =50)
+
+        senha = tkinter.Label(frame_baixo, text="Senha *", anchor=NW, font=('Ivy', 10), bg='#feffff', fg=c_pri)
+        senha.place(x=10, y =95)
+
+        passwd = tkinter.Entry(frame_baixo, width=25, justify='left', show='*', font=("", 15), highlightthickness=1, relief='solid')
+        passwd.place(x=14, y =125)
+
+        btn = tkinter.Button(frame_baixo, text="Entrar", width=34, height=2, bg=c_sec, fg=branco, font=('Ivy 10 bold'), relief=FLAT, command=lambda: verifica_login(janela, login, passwd))
+        btn.place(x=14, y =170)
 
 def verifica_login(janela, nConta, senha):
         
@@ -39,7 +62,7 @@ def verifica_login(janela, nConta, senha):
             if senha.get() == senha_bd[0][0]:
                 tela_incial.janelaEntrar(janela, nConta)
             else:
-                janelaLogin(janela, message="Senha inválida! Tente novamente")
+                messagebox.showwarning('', 'Senha inválida! Tente novamente')
         else:
-            janelaLogin(janela, message="Usuário inválido! Tente novamente")
+            messagebox.showwarning('', 'Usuário inválido! Tente novamente')
 
