aada7931e982193fb8c567024f20713ff3697180
Larabsg <laragomes2009@hotmail.com>
2022-05-28 02:17:09 -0300

    estilizando tela inicial e de extrato

--- sistema-bancario/tela_extrato.py
+++ sistema-bancario/tela_extrato.py
@@ -5,7 +5,18 @@ from tokenize import Double
 from connection_sqlite import *
 
 from tkinter import *
+import tkinter
 from tkinter.ttk import *
+
+c_pri = "#2d6375"
+branco = "#D7E0D7"
+
+preta = "#f0f3f5"
+verde = "#3fb5a3"
+letra = "#403d3d"
+valor = "#38576b"
+azul = "#00008e"
+c_sec = "#193842"
 
 def extrato(nConta):
         user_list = []
@@ -21,14 +32,21 @@ def janelaExtrato(janela, nConta):
         text_extrato = extrato(nConta)
         # lembrar de verificar qual o tipo de conta
         janela6 = Toplevel(janela)
-        janela6.title("SGB ")
-        janela6.geometry("300x240")
+        janela6.geometry("310x300")
+
+        frame_cima = tkinter.Frame(janela6, width=310, height=50, relief='flat', bg=c_pri)
+        frame_cima.grid(row=0, column=0, pady=0, padx=0, sticky=NSEW)
+        frame_baixo = tkinter.Frame(janela6, width=310, height=250, relief='flat', bg=c_pri)
+        frame_baixo.grid(row=1, column=0, pady=0, padx=0, sticky=NSEW)
         
-        texto = Label(janela6, text=" Extrato ")
-        texto.place(x=70, y=20)
+        texto = tkinter.Label(frame_cima, text="EXTRATO", anchor=NE, font=('Ivy', 18), bg=c_pri, fg=branco)
+        texto.place(x=5, y=5)
+
+        linha = tkinter.Label(frame_cima, text="", anchor=NW, width=275, font=('Ivy 1'), bg=branco, fg=letra)
+        linha.place(x=10, y =45)
 
         index = 40
         for e in range(len(text_extrato)):
-            lista = Label(janela6, text=f'{text_extrato[e][0]} ........................ {text_extrato[e][1]}\n')
-            lista.place(x=70, y=index)
+            lista = tkinter.Label(frame_baixo, text=f'{text_extrato[e][0]} .............................................. {text_extrato[e][1]}\n', font=('Ivy 10 bold'), bg=c_pri, fg=branco)
+            lista.place(x=10, y=index)
             index+=20
--- sistema-bancario/tela_incial.py
+++ sistema-bancario/tela_incial.py
@@ -4,36 +4,55 @@ from tokenize import Double
 from connection_sqlite import *
 from tkinter import *
 from tkinter.ttk import *
+import tkinter
 import tela_sacar
 import tela_depositar
 import tela_extrato
 
+c_pri = "#2d6375"
+branco = "#D7E0D7"
+
+preta = "#f0f3f5"
+verde = "#3fb5a3"
+letra = "#403d3d"
+valor = "#38576b"
+azul = "#00008e"
+c_sec = "#193842"
 
 
-def janelaEntrar(janela, nConta):
+def janelaEntrar(janela, nConta=1234):
     
+    janela2 = Toplevel(janela)
+    #janela2.title(f"Bem Vindo, {info[0][0]}! ")
+    janela2.geometry("310x300")
+
+    frame_cima = tkinter.Frame(janela2, width=310, height=50, relief='flat', bg=c_pri)
+    frame_cima.grid(row=0, column=0, pady=0, padx=0, sticky=NSEW)
+    frame_baixo = tkinter.Frame(janela2, width=310, height=250, relief='flat', bg=c_pri)
+    frame_baixo.grid(row=1, column=0, pady=0, padx=0, sticky=NSEW)
+
     cur.execute(f'select nome, saldo from conta where nConta = {nConta}')
     info = cur.fetchall()
-    janela2 = Toplevel(janela)
-    janela2.title(f"Bem Vindo, {info[0][0]}! ")
-    janela2.geometry("300x240")
 
-    texto = Label(janela2, text=f"Bem Vindo, {info[0][0]}! ")
-    texto.place(x=70, y=20)
+    texto = tkinter.Label(frame_cima, text=f"Olá, {info[0][0]}! ", anchor=NE, font=('Ivy', 18), bg=c_pri, fg=branco)
+    texto.place(x=5, y=5)
 
-    texto_saldo = Label(janela2, text=f"Saldo = {info[0][1]}")
-    texto_saldo.place(x=70, y=40)
+    linha = tkinter.Label(frame_cima, text="", anchor=NW, width=275, font=('Ivy 1'), bg=branco, fg=letra)
+    linha.place(x=10, y =45)
 
-    btnSacar = Button(janela2, text="Sacar", command=partial(
+    texto_saldo = tkinter.Label(frame_baixo, text=f"Saldo: {info[0][1]}", font=('Ivy 18'), bg=c_pri, fg=branco)
+    texto_saldo.place(x=10, y=20)
+
+    btnSacar = tkinter.Button(frame_baixo, text="Sacar", width=39, height=2, bg=c_sec, fg=branco, font=('Ivy 8 bold'), relief=FLAT, command=partial(
         tela_sacar.janelaSacar, janela, nConta))
-    btnSacar.place(x=100, y=100)
+    btnSacar.place(x=10, y=90)
 
-    btnDepositar = Button(janela2, text="Depositar", command=partial(
+    btnDepositar = tkinter.Button(frame_baixo, text="Depositar", width=39, height=2, bg=c_sec, fg=branco, font=('Ivy 8 bold'), relief=FLAT, command=partial(
         tela_depositar.janelaDepositar, janela, nConta))
-    btnDepositar.place(x=50, y=150)
+    btnDepositar.place(x=10, y=140)
 
-    btnExtrato = Button(janela2, text="Extrato", command=partial(
+    btnExtrato = tkinter.Button(frame_baixo, text="Extrato", width=39, height=2, bg=c_sec, fg=branco, font=('Ivy 8 bold'), relief=FLAT, command=partial(
         tela_extrato.janelaExtrato, janela, nConta))
-    btnExtrato.place(x=160, y=150)
+    btnExtrato.place(x=10, y=190)
 
     
