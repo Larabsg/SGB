7212b541c7a237d630be07c637b026de388b111e
Larabsg <laragomes2009@hotmail.com>
2022-06-04 12:44:39 -0300

    commit aleatorio

--- sistema-bancario/ContaCorrente.py
+++ sistema-bancario/ContaCorrente.py
@@ -3,39 +3,46 @@ from connection_sqlite import *
 
 
 class ContaCorrente(Conta.Conta):
-    def __init__(self, nConta, saldo, cpf, nome, senha, saque):
-        super().__init__(nConta, saldo, cpf, nome, senha)
-        self.__saque = saque
+    def __init__(self, nConta, saldo, cpf, nome, senha, tipoConta):
+        super().__init__(nConta, saldo, cpf, nome, senha, tipoConta)
 
-    def get_saque(self):
-        return self.__saque
+#         self.__saque = saque
 
-    def set__saque(self, saque):
-        self.__saque = saque
+#     def get_saque(self):
+#         return self.__saque
+
+#     def set__saque(self, saque):
+#         self.__saque = saque
 
     def sacar(self, saque, nConta):
-        
-        user_list = [] 
+
+        user_list = []
         cur.execute(f'select * from conta where nConta = {nConta}')
-        
-        saldo = user_list[0][5]
-        
-        if(saque >= saldo):
-            # atualiza para zero o saldo
-            # é preciso fazer uma consulata novamente no banco
-            cur.execute(
-                f'UPDATE conta SET saldo = {0} where nConta = {nConta}')
-            cur.execute(
-                f'INSERT INTO transacao (nconta, tipo, valor) VALUES ({nConta}, "Saque", {saque});')
-            con_sqlite.commit()
-            print("saldo zero")
-        else:
 
-            saldo = (saldo - saque)
-            cur.execute(
-                f'UPDATE conta SET saldo = {saldo} WHERE nConta = {nConta};')
+        for x in cur:
+            user_list.append(x)
 
-            cur.execute(
-                f'INSERT INTO transacao (nconta, tipo, valor) VALUES ({nConta}, "Saque", {saque});')
-            con_sqlite.commit()
-            print('Saque efetuado com sucesso')
+        if user_list.__len__() == 1:
+            saldo = user_list[0][5]
+
+            if(saque >= saldo):
+                # atualiza para zero o saldo
+                # é preciso fazer uma consulata novamente no banco
+                saldo = (saldo-saque)
+                print(saldo)
+                cur.execute(
+                    f'UPDATE conta SET saldo = {saldo} where nConta = {nConta}')
+                cur.execute(
+                    f'INSERT INTO transacao (nconta, tipo, valor) VALUES ({nConta}, "Saque", {saque});')
+                con_sqlite.commit()
+                print("saldo zero")
+            else:
+
+                saldo = (saldo - saque)
+                cur.execute(
+                    f'UPDATE conta SET saldo = {saldo} WHERE nConta = {nConta};')
+
+                cur.execute(
+                    f'INSERT INTO transacao (nconta, tipo, valor) VALUES ({nConta}, "Saque", {saque});')
+                con_sqlite.commit()
+                print('Saque efetuado com sucesso')
--- sistema-bancario/tela_sacar.py
+++ sistema-bancario/tela_sacar.py
@@ -24,7 +24,8 @@ def sacar(valor, nConta):
             saldo = user_list[0][5]
             
             if(cur.execute(f'select * from conta where nConta = {nConta} and tipoConta = "Corrente" ')):
-                c1 = ContaCorrente.sacar(valor, nConta)
+                c1 = ContaCorrente(nConta, 0, '0', '0', '0', '0')
+                c1.sacar(valor,nConta)
                 
             else:
                 if(saldo-valor) < 0:
