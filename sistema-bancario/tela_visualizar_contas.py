5fa21d4cb5a221dd212b41380fac5a524970df3f
Larabsg <laragomes2009@hotmail.com>
2022-06-08 20:11:00 -0300

    scrollbar em visualização de contas - incompleto

--- sistema-bancario/tela_visualizar_contas.py
+++ sistema-bancario/tela_visualizar_contas.py
@@ -2,11 +2,13 @@ from cgitb import text
 from ctypes.wintypes import DOUBLE
 from functools import partial
 from tokenize import Double
+from turtle import left
 from connection_sqlite import *
 
 from tkinter import *
 import tkinter
 from tkinter.ttk import *
+from tkinter import ttk
 
 c_pri = "#2d6375"
 branco = "#D7E0D7"
@@ -20,11 +22,18 @@ def janelaVisualizarContas(contas):
         janela6 = Tk()
         janela6.geometry("310x300")
 
+
         frame_cima = tkinter.Frame(janela6, width=310, height=50, relief='flat', bg=c_pri)
         frame_cima.grid(row=0, column=0, pady=0, padx=0, sticky=NSEW)
         frame_baixo = tkinter.Frame(janela6, width=310, height=250, relief='flat', bg=c_pri)
         frame_baixo.grid(row=1, column=0, pady=0, padx=0, sticky=NSEW)
-        
+
+        #frame_baixo.config(yscrollcommand=scrollbar.set)
+        # scrollbar.config(command=frame_baixo.yview)
+        scrollbar = tkinter.Scrollbar(frame_baixo)
+        scrollbar.pack(side="right", fill=Y)
+
+
         texto = tkinter.Label(frame_cima, text="CONTAS", anchor=NE, font=('Ivy', 18), bg=c_pri, fg=branco)
         texto.place(x=5, y=5)
 
@@ -34,7 +43,13 @@ def janelaVisualizarContas(contas):
         index = 40
         txt = tkinter.Label(frame_baixo, text='NOME \t|\t CPF \t|\t SALDO', font=('Ivy 10 bold'), bg=c_pri, fg=branco)
         txt.place(x=15, y=10)
+
+        lista = tkinter.Listbox(frame_baixo, yscrollcommand=scrollbar.set, font=('Ivy 10 bold'), bg=c_pri, fg=branco)
         for e in range(len(contas)):
-            lista = tkinter.Label(frame_baixo, text=f'{contas[e][1]} \t| {contas[e][2]} \t| {contas[e][5]}\n', font=('Ivy 10 bold'), bg=c_pri, fg=branco)
-            lista.place(x=10, y=index)
-            index+=20
+            lista.insert(END, f"{contas[e][1]} \t| {contas[e][2]} \t| {contas[e][5]}\n")
+            # lista.place(x=10, y=index)
+            # index+=20
+        lista.pack(side=LEFT, fill=BOTH, expand=True)
+        # lista.place(x=10, y=40)
+        #lista.place(x=10, y=40)
+        scrollbar.config(command=lista.yview)
