75f5ab8b5a0094684252b475dc9b3ff0c6dd18e3
melsousa <melissasousa6ms@gmail.com>
2022-06-08 20:07:50 -0300

    implementação do método emprestimo 70%

--- sistema-bancario/Autenticavel.py
+++ sistema-bancario/Autenticavel.py
@@ -1,9 +1,18 @@
 from abc import ABC, abstractmethod
 
-class Autenticavel(ABC):
-    
-    @abstractmethod
+class Autenticavel():
+    def __init__(self, senha):
+        self.__senha = senha
+
+    def get_senha(self):
+        return self.__senha
+
+    def set_senha(self, senha):
+        self.__senha = senha
+
+    # @abstractmethod
     def autentica(self):
+        #verifica se a senha está correta
         pass
 
 # autenticaveis = ["gerente", "diretor", "cliente"]
